dc03a4a3c566c61f445a468a90c96f8ba32dd793
Jakub Groncki <jakub.groncki@bitbag.pl>
2022-01-25 13:47:19 +0100

    Simplify ApiException.php

--- src/AppSystem/Exception/ApiException.php
+++ src/AppSystem/Exception/ApiException.php
@@ -8,18 +8,8 @@ use Psr\Http\Message\ResponseInterface;
 
 class ApiException extends \Exception
 {
-    private string $requestPath;
-
-    private string $shopUrl;
-
-    private ResponseInterface $response;
-
     public function __construct(string $shopUrl, string $requestPath, ResponseInterface $response)
     {
-        $this->requestPath = $requestPath;
-        $this->shopUrl = $shopUrl;
-        $this->response = $response;
-
         $message = \sprintf(
             'Error occurred while requesting %s from shop %s, got status %s and response was %s',
             $requestPath,
@@ -28,21 +18,6 @@ class ApiException extends \Exception
             $response->getBody()->getContents()
         );
 
-        parent::__construct($message, 0, null);
-    }
-
-    public function getRequestPath(): string
-    {
-        return $this->requestPath;
-    }
-
-    public function getShopUrl(): string
-    {
-        return $this->shopUrl;
-    }
-
-    public function getResponse(): ResponseInterface
-    {
-        return $this->response;
+        parent::__construct($message);
     }
 }
