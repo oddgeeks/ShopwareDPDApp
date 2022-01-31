76e154860c330a226055acc8aa3d6344ea77e2dd
Jakub Groncki <jakub.groncki@bitbag.pl>
2022-01-31 14:10:17 +0100

    Fix deprecations

--- src/Kernel.php
+++ src/Kernel.php
@@ -10,7 +10,6 @@ use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
-use Symfony\Component\Routing\RouteCollectionBuilder;
 
 class Kernel extends BaseKernel
 {
