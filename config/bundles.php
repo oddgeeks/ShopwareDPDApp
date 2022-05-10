746752c8f3fb20a10c70260f63c0ad667e08a595
Norbert Glanc <norglanc@gmail.com>
2022-05-10 15:03:43 +0200

    Remove AppSystem from src directory, add AppSystem from composer

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    BitBag\ShopwareAppSystemBundle\BitBagShopwareAppSystemBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
--- src/AppSystem/LifecycleEvent/AppDeletedEvent.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace BitBag\ShopwareDpdApp\AppSystem\LifecycleEvent;
-
-final class AppDeletedEvent extends AbstractLifecycleEvent
-{
-}
