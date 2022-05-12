d4b6b6889920541cc3ef5b28b5218b926b2b3b90
Norbert Glanc <17459070+Glancu@users.noreply.github.com>
2022-05-12 10:00:45 +0200

    Update src/Entity/Config.php
    Co-authored-by: Piotr Szostakiewicz <63583880+pszost@users.noreply.github.com>

--- src/Entity/Config.php
+++ src/Entity/Config.php
@@ -51,7 +51,7 @@ class Config implements ConfigInterface
 
     /**
      * @ORM\OneToOne(targetEntity="BitBag\ShopwareAppSystemBundle\Entity\Shop", cascade={"persist", "remove"})
-     * @ORM\JoinColumn(name="shop_id", referencedColumnName="shop_id", nullable=false)
+    * @ORM\JoinColumn(nullable=false, referencedColumnName="shop_id", onDelete="CASCADE")
      */
     protected ShopInterface $shop;
 
