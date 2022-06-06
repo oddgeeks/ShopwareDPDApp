024304d6f4417884752bc96b9965b12483c80500
Norbert Glanc <norglanc@gmail.com>
2022-06-06 07:58:10 +0200

    Change entity mapping to xml

--- src/Entity/Config.php
+++ src/Entity/Config.php
@@ -5,57 +5,34 @@ declare(strict_types=1);
 namespace BitBag\ShopwareDpdApp\Entity;
 
 use BitBag\ShopwareAppSystemBundle\Entity\ShopInterface;
-use BitBag\ShopwareDpdApp\Repository\ConfigRepository;
-use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass=ConfigRepository::class)
- * @ORM\Table(name="config")
  * @psalm-suppress MissingConstructor
  */
 class Config implements ConfigInterface
 {
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
     protected int $id;
 
-    /** @ORM\Column(type="string", nullable=true) */
     protected string $apiLogin;
 
-    /** @ORM\Column(type="string", nullable=true) */
     protected string $apiPassword;
 
-    /** @ORM\Column(type="string", nullable=true) */
     protected string $apiFid;
 
-    /** @ORM\Column(type="string", nullable=true) */
     protected string $apiEnvironment;
 
-    /** @ORM\Column(type="string", nullable=true) */
     protected string $senderFirstLastName;
 
-    /** @ORM\Column(type="string", nullable=true) */
     protected string $senderStreet;
 
-    /** @ORM\Column(type="string", nullable=true) */
     protected string $senderZipCode;
 
-    /** @ORM\Column(type="string", nullable=true) */
     protected string $senderCity;
 
-    /** @ORM\Column(type="string", nullable=true) */
     protected string $senderPhoneNumber;
 
-    /** @ORM\Column(type="string", nullable=true) */
     protected string $senderLocale;
 
-    /**
-     * @ORM\OneToOne(targetEntity="BitBag\ShopwareAppSystemBundle\Entity\Shop", cascade={"persist", "remove"})
-     * @ORM\JoinColumn(nullable=false, referencedColumnName="shop_id", onDelete="CASCADE")
-     */
     protected ShopInterface $shop;
 
     public function getId(): ?int
