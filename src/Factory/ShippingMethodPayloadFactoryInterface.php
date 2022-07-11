86178bb3bf1a887e3d150654fcf4bba077ad4ffd
Norbert Glanc <norglanc@gmail.com>
2022-07-11 09:09:23 +0200

    Remove code for create shipping method, add twig scripts for create shipping method

--- src/Factory/ShippingMethodPayloadFactoryInterface.php
+++ src/Factory/ShippingMethodPayloadFactoryInterface.php
@@ -4,15 +4,7 @@ declare(strict_types=1);
 
 namespace BitBag\ShopwareDpdApp\Factory;
 
-use Vin\ShopwareSdk\Repository\Struct\IdSearchResult;
-
 interface ShippingMethodPayloadFactoryInterface
 {
     public const SHIPPING_KEY = 'DPD';
-
-    public function create(
-        string $ruleId,
-        string $currencyId,
-        IdSearchResult $deliveryTime
-    ): array;
 }
